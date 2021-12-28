c1844e827323c5b04839e21c72462469b175691e
tanvinsharma <tanvin2000s@gmail.com>
2021-12-28 03:07:32 +0100

    some changes

--- main.py
+++ main.py
@@ -76,16 +76,3 @@ def solovoyStrassen(p, iterations):
         if jacobian == 0 or mod != jacobian:
             return False
     return True
-
-
-# Test Code
-# iterations = 50
-# num = [14, 11, 20, 83, 103, 305, 654, 1259, 8493, 29034]
-
-# for i in range(len(num)):
-#     if solovoyStrassen(num[i], iterations):
-#         print(num[i], "is prime")
-#     else:
-#         print(num[i], "is composite")
-#     print("Prime? ", sympy.isprime(num[i]))
-#     print()
